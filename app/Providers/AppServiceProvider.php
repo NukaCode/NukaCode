501afc187253155569feb06bd49d121011d38d0d
Travis Blasingame <stygian.warlock.v2@gmail.com>
2016-03-04 15:58:38 -0600

    Update AppServiceProvider.php

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -23,7 +23,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        if (! $this->app->environment('production')) {
+        if ($this->app->environment('local') || Input::get('debug') == true) {
             $this->app->register(\Barryvdh\Debugbar\ServiceProvider::class);
             $this->app->register(\Barryvdh\LaravelIdeHelper\IdeHelperServiceProvider::class);
         }
